910818c5e71e9c3890d5068ce8ff44a52ddb226c
pncontemplacion <paulbcon@hotmail.com>
2019-04-20 20:46:14 -0400

    config.js - hiding the API key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization:
-          "Client-ID c7f23fd87d1edb26bc59636355d64f0b6de5d0dca495c71beb361af876041137"
+          "Client-ID " // use your Client ID API key from unsplash.com
       }
 });
